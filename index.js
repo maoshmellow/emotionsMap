11c8b8005afc97beb7199c268562f78e10b64d82
Josh Mao <96925125+maoshmellow@users.noreply.github.com>
2022-03-03 14:15:03 -0800

    added geolocation functionality

--- index.js
+++ index.js
@@ -8,6 +8,37 @@ var globeContainer = document.getElementById("globeContainer");
 var containerWidth = globeContainer.offsetWidth;
 var containerHeight = globeContainer.offsetHeight;
 
+const globeButton = document.getElementById("globeButton");
+
+
+
+function findState(){
+  const state = document.getElementById("locationText");
+
+  function success(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+    fetch(geoApiUrl)
+    .then(res => res.json())
+    .then(data => {
+      state.textContent = data.principalSubdivision;
+      console.log(data);
+    })
+  }
+  
+  function error(){
+    state.textContent = "xxx";
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+
+
+globeButton.addEventListener("click", findState);
+
 const N = 300;
 const gData = [...Array(N).keys()].map(() => ({
     lat: (Math.random() - 0.5) * 180,
